62882aec1765c9a0c22fbf963fa3b22cbaff9ab9
IT24102367 <it24102367@my.sliit.lk>
2024-08-17 20:33:23 +0530

    IT24102367 Aththanayaka A.M.S.M
    Lab4Q1

---
+++ Q1/IT24102367Lab4Q1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class IT24102367Lab4Q1{
+
+	public static void main(String[] args){
+	
+	Scanner input = new Scanner(System.in);
+
+	int number;
+
+	System.out.print("Enter a number : ");
+	number = input.nextInt();
+	
+	if(number > 0) {
+ 		
+		System.out.println("The number is:positive"); }
+		
+		else if(number < 0) {
+		
+		System.out.println( "The number is:Negative "); }
+
+		else {
+
+		System.out.println( "The number is zero "); }
+		}				
+	}	
